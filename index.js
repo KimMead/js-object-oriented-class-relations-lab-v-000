7685b90059b571e09497e53dca899fc8f991b9db
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-23 16:41:38 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+// A driver has mayn trips and has many passengers through trips.
+class Driver{
+  constructor(name){
+    this.name = name; 
+    this.id = driverId++; 
+    store.drivers.push(this);
+  }
+}
+  
