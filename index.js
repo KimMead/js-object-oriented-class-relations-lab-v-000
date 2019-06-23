d85f734f4ec2c4a0a14fe39fbbe6391c17af348b
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-23 16:46:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // A driver has mayn trips and has many passengers through trips.
 let store = { drivers: [], passengers: [], trips: [] };
 
-let driverId = 0; 
-let passengerId = 0; 
-let tripId = 0; 
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
 
 class Driver{
   constructor(name){
